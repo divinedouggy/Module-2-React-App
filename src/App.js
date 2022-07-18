2ac3d505b143cec33e7d86388947b5e90710fb96
divinedouggy <105749979+divinedouggy@users.noreply.github.com>
2022-07-18 14:02:43 -0400

    rendered ingredients

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {NavLink, Route, Routes} from 'react-router-dom'
+import Home from './screens/Home';
+import About from './screens/About'
+import Ingredients from './screens/Ingredients';
+import NotFound from './screens/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='links'>
+        <nav>
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+        <NavLink to={"/ingredients"}>Ingredients</NavLink>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/ingredients" element={<Ingredients/>}/>
+        <Route path="*" element={<NotFound/>}/>
+
+      </Routes>
+
     </div>
   );
 }
---
+++ src/screens/NotFound.js
@@ -0,0 +1,12 @@
+
+
+function NotFound() {
+    return(
+        <div>
+            Page Not Found
+
+        </div>
+    )
+}
+
+export default NotFound
